b760634878552c9ae446abec9c13c46dfde7785a
Rayhan Pervej <103215390+Rayhan-Pervej@users.noreply.github.com>
2025-02-06 01:53:52 +0600

    updated

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     domains: ['cdn.pixabay.com', 'fabrilife.com'],
-
   },
+  output: 'export',  // This enables static export
 };
 
 export default nextConfig;
