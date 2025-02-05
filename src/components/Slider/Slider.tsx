f8b4c83aaee645293ef0b6bd2d1d235bb0a4b7be
Rayhan Pervej <103215390+Rayhan-Pervej@users.noreply.github.com>
2025-02-06 01:50:16 +0600

    updated

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -1,7 +1,7 @@
 "use client"
 import Link from 'next/link';
 import Image from 'next/image'
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 const slides = [
     {
         id: 1,
