7344c230264bbfdef31a6184f8dc41dcc67eb2d0
Rayhan Pervej <103215390+Rayhan-Pervej@users.noreply.github.com>
2025-02-06 01:40:13 +0600

    updated

--- src/components/Navbar/SearchBar.tsx
+++ src/components/Navbar/SearchBar.tsx
@@ -6,15 +6,13 @@ import { useRouter } from 'next/navigation';
 
 
 const SearchBar = () => {
-    const router = useRouter();
+
     const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
 
         e.preventDefault();
         const formData = new FormData(e.currentTarget);
         const name = formData.get("name") as string;
-        if (name) {
-            router.push(`/list?name=${name}`)
-        };
+
 
     };
     return (
