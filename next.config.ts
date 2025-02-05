2e96edada0ee5cb2042f2181f679e186dde97311
Rayhan Pervej <103215390+Rayhan-Pervej@users.noreply.github.com>
2025-02-06 01:29:54 +0600

    update

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  basePath: '/kisukinen_website',
   images: {
     domains: ['cdn.pixabay.com', 'fabrilife.com'],
-    
+
   },
 };
 
